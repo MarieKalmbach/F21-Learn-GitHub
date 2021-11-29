4400b0a20a58d6d41446695faa85aab063e6bb97
Marie Kalmbach <Marie.Kalmbach@gmail.com>
2021-11-29 09:47:22 -0600

    Initial Commit

---
+++ F21-Learn-GitHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  F21-Learn-GitHub
+//
+//  Created by BPSTIL-Pascal-KALMBACH  on 11/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ F21-Learn-GitHub/F21_Learn_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  F21_Learn_GitHubApp.swift
+//  F21-Learn-GitHub
+//
+//  Created by BPSTIL-Pascal-KALMBACH  on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct F21_Learn_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
