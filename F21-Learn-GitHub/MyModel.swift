12968c96a9d925a61b3056a2a457d2154edc65c5
Marie Kalmbach <Marie.Kalmbach@gmail.com>
2021-11-29 15:44:32 -0600

    Add MyModel.swift to the project

---
+++ F21-Learn-GitHub/MyModel.swift
@@ -0,0 +1,20 @@
+//
+//  MyModel.swift
+//  F21-Learn-GitHub
+//
+//  Created by BPSTIL-Pascal-KALMBACH  on 11/29/21.
+//
+
+import SwiftUI
+
+struct MyModel: View {
+    var body: some View {
+        Text( "Hello, My Model.  What's up?")
+    }
+}
+
+struct MyModel_Previews: PreviewProvider {
+    static var previews: some View {
+        MyModel()
+    }
+}
